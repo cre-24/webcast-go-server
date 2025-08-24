31f3383012fb4711de9811ce13c18a98ec3a0bb7
Chris <emerick@Mac.lan>
2025-08-24 13:35:17 -0400

    Get file path fro CLI

--- webcast.go
+++ webcast.go
@@ -1,13 +1,15 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 	"os"
 	"os/exec"
 )
 
-var videoFile = "" // Video file path
+var videoFile string
+
 func streamHandler(w http.ResponseWriter, r *http.Request) {
 	// Check if the video file exists
 	if _, err := os.Stat(videoFile); os.IsNotExist(err) {
@@ -31,6 +33,16 @@ func streamHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	// Get the video file path from command line arguments
+	// Define a command-line flag for the video file path
+	flag.StringVar(&videoFile, "file", "", "Path to the video file")
+	flag.Parse()
+
+	// Check if the video file path is provided
+	if videoFile == "" {
+		log.Fatal("Video file path must be provided using the -file flag")
+	}
+
 	http.HandleFunc("/stream", streamHandler) // Handler for streaming
 	log.Println("Starting server on :8080")
 	log.Fatal(http.ListenAndServe(":8080", nil)) // Start the server
